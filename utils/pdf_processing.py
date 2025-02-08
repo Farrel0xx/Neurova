5fb79af2b7ab613d60581e17b0223d23bdd33068
You miss me. <ghost404sec@gmail.com>
2025-02-07 22:13:16 -0500

    Update pdf_processing.py

--- utils/pdf_processing.py
+++ utils/pdf_processing.py
@@ -1,7 +1,6 @@
 import pdfplumber
 
 def extract_text_from_pdf(uploaded_file):
-    """Mengekstrak teks dari file PDF menggunakan pdfplumber"""
     text = ""
     with pdfplumber.open(uploaded_file) as pdf:
         for page in pdf.pages:
