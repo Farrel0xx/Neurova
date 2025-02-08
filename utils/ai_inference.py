43318e822bfb560366ea2daa581cdfcf9d3a3ace
You miss me. <ghost404sec@gmail.com>
2025-02-07 22:13:05 -0500

    Update ai_inference.py

--- utils/ai_inference.py
+++ utils/ai_inference.py
@@ -2,17 +2,14 @@ import google.generativeai as genai
 from langchain_groq import ChatGroq
 from config import GOOGLE_API_KEY, GROQ_API_KEY
 
-# Setup Google Gemini API
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# Setup Llama (Groq)
 llm = ChatGroq(
     groq_api_key=GROQ_API_KEY,
     model="llama-3.2-1b-preview"
 )
 
 def get_response(content, prompt):
-    """Menghasilkan respon dari model Gemini"""
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(
         [content, prompt], 
